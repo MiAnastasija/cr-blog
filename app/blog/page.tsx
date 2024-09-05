87da6434e78cd1c0eda97e3ba2052ec8aaa6387d
Anastasija <anastasija.miladinovic@gmail.com>
2024-09-05 11:01:03 +0200

    blog strana

---
+++ app/blog/page.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from '../components/Navbar';
+
+const Blog: React.FC = () => {
+    return (
+        <div className="mx-auto">
+      <Navbar />
+      <div className="pt-20 ">
+        <div className=" max-w-sirina mx-auto ">
+        <h1 className="text-4xl font-bold text-center mt-8">Blog</h1>
+        </div>
+        </div>
+      </div>
+    );
+};
+export default Blog;
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { post } from './Schemas/post'
 import { paketi } from './Schemas/paketi'
 import { blogPost } from './Schemas/blogPost'
+import  {blockContent } from './Schemas/blockContent'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post,paketi, blogPost],
+  types: [post,paketi, blogPost, blockContent],
 }
